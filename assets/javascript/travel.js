9d7ddd6fd4cb18c94e6bddc2e0bd010e5145a972
emssslay <emily.robinson@iechs.org>
2019-03-22 13:07:06 -0600

    starting files

---
+++ assets/javascript/travel.js
@@ -0,0 +1,16 @@
+  // Initialize Firebase
+  var config = {
+      apiKey: "AIzaSyCcRG_GwiN8sBxD8xwcMEttzby0cND52KY",
+      authDomain: "travel-project-ac0c2.firebaseapp.com",
+      databaseURL: "https://travel-project-ac0c2.firebaseio.com",
+      projectId: "travel-project-ac0c2",
+      storageBucket: "travel-project-ac0c2.appspot.com",
+      messagingSenderId: "549559435677"
+  };
+
+  firebase.initializeApp(config);
+  var database = firebase.database();
+
+
+  // Initial Values
+  

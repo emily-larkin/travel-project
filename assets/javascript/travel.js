95f04ce27f8e5c7934b5c8b038bef3705cf78340
Michael Olsen <michaelolsen@michaels-air.uconnect.utah.edu>
2019-03-23 12:31:28 -0600

    stuff

--- assets/javascript/travel.js
+++ assets/javascript/travel.js
@@ -64,7 +64,8 @@ $( document ).ready( function () {
       for ( var i = 0; i < response.data.length; i++ ) {
 
         var image = $( "<img src= " + response.data[i].images.fixed_width.url + ">" )
-          ( "#display" ).append( image );
+          ( "#hiking" ).append( image );
+        console.log( image )
       }
     } )
   };
@@ -90,67 +91,67 @@ $( document ).ready( function () {
 
 } );
 
-$(document).ready(function () {
-    // GLOBAL VARIABLES
+$( document ).ready( function () {
+  // GLOBAL VARIABLES
 
-    // Initial Values
-
-
-    // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyCcRG_GwiN8sBxD8xwcMEttzby0cND52KY",
-        authDomain: "travel-project-ac0c2.firebaseapp.com",
-        databaseURL: "https://travel-project-ac0c2.firebaseio.com",
-        projectId: "travel-project-ac0c2",
-        storageBucket: "travel-project-ac0c2.appspot.com",
-        messagingSenderId: "549559435677"
-    };
-
-    firebase.initializeApp(config);
-    var database = firebase.database();
+  // Initial Values
 
 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCcRG_GwiN8sBxD8xwcMEttzby0cND52KY",
+    authDomain: "travel-project-ac0c2.firebaseapp.com",
+    databaseURL: "https://travel-project-ac0c2.firebaseio.com",
+    projectId: "travel-project-ac0c2",
+    storageBucket: "travel-project-ac0c2.appspot.com",
+    messagingSenderId: "549559435677"
+  };
 
-    // FUNCTIONS 
-
-    // on click function to call the api, should use data- to input which activity is being clicked or searched
-    $("????").on("click", function () {
-        // assigns the data attribute
-        var sdfdf = $(this).attr("data-sdfdf");
-        // &limit=10 limits the response by 10 
-        activityQueryURL = 'http://api.amp.active.com/v2/search&q=' + sdfdf + 'near=Salt%20Lake%20City,UT,US&show_distance=true&sort=distance&api_key=&limit=15';
-
-        // link the api using ajax
-        $.ajax({
-                url: activityQueryURL,
-                method: "GET"
-            })
-            .then(function (response) {
-                var results = response.data;
-                $("#????").empty();
-                for (var i = 0; i < results.length; i++) {
-                    // append location 
-                    // example: $("#giphy").prepend("<p>Rating of Giph: " + results[i].rating + "</p>");
-                    // append distance 
-
-                }
-            });
-    });
-
-    // snapshot to database
-    database.ref("???").on("value", function (snapshot) {
+  firebase.initializeApp( config );
+  var database = firebase.database();
 
 
-    
+
+  // FUNCTIONS 
+
+  // on click function to call the api, should use data- to input which activity is being clicked or searched
+  $( "????" ).on( "click", function () {
+    // assigns the data attribute
+    var sdfdf = $( this ).attr( "data-sdfdf" );
+    // &limit=10 limits the response by 10 
+    activityQueryURL = 'http://api.amp.active.com/v2/search&q=' + sdfdf + 'near=Salt%20Lake%20City,UT,US&show_distance=true&sort=distance&api_key=&limit=15';
+
+    // link the api using ajax
+    $.ajax( {
+      url: activityQueryURL,
+      method: "GET"
+    } )
+      .then( function ( response ) {
+        var results = response.data;
+        $( "#????" ).empty();
+        for ( var i = 0; i < results.length; i++ ) {
+          // append location 
+          // example: $("#giphy").prepend("<p>Rating of Giph: " + results[i].rating + "</p>");
+          // append distance 
+
+        }
+      } );
+  } );
+
+  // snapshot to database
+  database.ref( "???" ).on( "value", function ( snapshot ) {
+
+
+
     // If any errors are experienced, log them to console.
-    }, function (errorObject) {
-        console.log("The read failed: " + errorObject.code);
-    });
+  }, function ( errorObject ) {
+    console.log( "The read failed: " + errorObject.code );
+  } );
 
 
 
-    // MAIN PROCESS
+  // MAIN PROCESS
 
 
 
-});
+} );

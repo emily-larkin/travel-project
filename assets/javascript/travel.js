7d8558b692df80deef20b2ad10d972ba93b09790
Kayla Tryon <dawnka22@gmail.com>
2019-03-27 18:23:29 -0600

    got the dropdown working

--- assets/javascript/travel.js
+++ assets/javascript/travel.js
@@ -23,33 +23,42 @@ $( document ).ready( function () {
   // FUNCTIONS
 
   // Dropdown trigger
-  // $('.dropdown-trigger').dropdown().open();
-
+  // (function($) {
+  //   $(function() {
+ 
+  //     $('.button-collapse').sideNav();
+  //     $('select').material_select();
+  //     $('.dropdown-button').dropdown();
+ 
+  //   }); // end of document ready
+  // })(jQuery); // end of jQuery name space
+ 
+  $('.dropdown-trigger').dropdown();
 
 
   // on click function to call the api, should use data- to input which activity is being clicked or searched
-  $( "????" ).on( "click", function () {
-    // assigns the data attribute
-    var sdfdf = $( this ).attr( "data-sdfdf" );
-    // &limit=10 limits the response by 10
-    activityQueryURL = 'http://api.amp.active.com/v2/search&q=' + sdfdf + 'near=Salt%20Lake%20City,UT,US&show_distance=true&sort=distance&api_key=&limit=15';
+  // $( "????" ).on( "click", function () {
+  //   // assigns the data attribute
+  //   var sdfdf = $( this ).attr( "data-sdfdf" );
+  //   // &limit=10 limits the response by 10
+  //   activityQueryURL = 'http://api.amp.active.com/v2/search&q=' + sdfdf + 'near=Salt%20Lake%20City,UT,US&show_distance=true&sort=distance&api_key=&limit=15';
 
-    // link the api using ajax
-    $.ajax( {
-      url: activityQueryURL,
-      method: "GET"
-    } )
-      .then( function ( response ) {
-        var results = response.data;
-        $( "#????" ).empty();
-        for ( var i = 0; i < results.length; i++ ) {
-          // append location
-          // example: $("#giphy").prepend("<p>Rating of Giph: " + results[i].rating + "</p>");
-          // append distance
+  //   // link the api using ajax
+  //   $.ajax( {
+  //     url: activityQueryURL,
+  //     method: "GET"
+  //   } )
+  //     .then( function ( response ) {
+  //       var results = response.data;
+  //       $( "#????" ).empty();
+  //       for ( var i = 0; i < results.length; i++ ) {
+  //         // append location
+  //         // example: $("#giphy").prepend("<p>Rating of Giph: " + results[i].rating + "</p>");
+  //         // append distance
 
-        }
-      } );
-  } );
+  //       }
+  //     } );
+  // } );
 
 
   // Function to get giphy api's using user input(?)

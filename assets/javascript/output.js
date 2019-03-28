ec7ad4b36f91f5602805e8085878259287af555a
emssslay <emily.robinson@iechs.org>
2019-03-27 19:22:08 -0600

    committing api key

--- assets/javascript/output.js
+++ assets/javascript/output.js
@@ -5,6 +5,7 @@
 var activity =JSON.parse(sessionStorage.getItem("activityJSON"));
 
 for ( var i = 0; i < 10; i++ ) {
+    event.preventDefault();
     // append location
     // example: $("#giphy").prepend("<p>Rating of Giph: " + results[i].rating + "</p>");
     var newItem = $("<div class = 'list-item'>")
--- assets/javascript/travel.js
+++ assets/javascript/travel.js
@@ -37,11 +37,11 @@ $(document).ready(function () {
   $('.dropdown-trigger').dropdown();
   
   // on click function to call the api, should use data- to input which activity is being clicked or searched
-  $(".active").on("click", function () {
+  $(".dropdown-content").on("click", function () {
     // assigns the data attribute
     var activitiesCall = $(this).attr("data-active");
     // &limit=10 limits the response by 10
-    activityQueryURL = 'http://api.amp.active.com/v2/search&q=' + activitiesCall + 'near=Salt%20Lake%20City,UT,US&show_distance=true&sort=distance&api_key=&limit=10';
+    activityQueryURL = 'http://api.amp.active.com/v2/search&q=' + activitiesCall + 'near=Salt%20Lake%20City,UT,US&show_distance=true&sort=distance&api_key=9deez853x9tvu4b2ycpsjs7m&limit=10';
 
     // link the api using ajax
     $.ajax({
@@ -50,7 +50,6 @@ $(document).ready(function () {
       })
       .then(function (response) {
         var results = response.data;
-        $(".list-item").empty();
         for (var i = 0; i < results.length; i++) {
           // append location
           // example: $("#giphy").prepend("<p>Rating of Giph: " + results[i].rating + "</p>");

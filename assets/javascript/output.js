bbe47bcfbc9d39b4650ce6debed351076e8e937a
Cyber Witch <no-reply>
2019-03-30 10:55:32 -0600

    made locations for list items

--- assets/javascript/output.js
+++ assets/javascript/output.js
@@ -2,43 +2,64 @@
 
 //Then when the query loads, make a for statement that creates the divs and put the activity data within them.
 
-var activity =sessionStorage.getItem("activity");
+var activity = sessionStorage.getItem("activity");
 
 console.log(activity);
 
-var ajaxurl = "https://cors-anywhere.herokuapp.com/http://api.amp.active.com/v2/search?query=" + activity + "&start_date="+ getTodaysDate()+"&near=Salt%20Lake%20City,UT,US&show_distance=true&sort=distance&api_key=9deez853x9tvu4b2ycpsjs7m"
+var ajaxurl = "https://cors-anywhere.herokuapp.com/http://api.amp.active.com/v2/search?query=" + activity + "&start_date=" + getTodaysDate() + "&near=Salt%20Lake%20City,UT,US&show_distance=true&sort=distance&api_key=9deez853x9tvu4b2ycpsjs7m"
 
 
 console.log(ajaxurl)
 $.ajax({
   url: ajaxurl,
   method: "GET"
-}).then(function(response){
+}).then(function (response) {
 
   console.log(response)
 
-  for ( var i = 0; i < response.results.length; i++ ) {
-    // append location
-    // example: $("#giphy").prepend("<p>Rating of Giph: " + results[i].rating + "</p>");
-    var newItem = $("<div class = 'list-item flex-container'>")
+  for (let i = 0; i < response.results.length; i++) {
 
-    newItem.append("<img class = 'thumbnail' src =" + response.results[i].assetImages[0].imageUrlAdr + " width ='70px' height = '70px' alt = 'img'>")
+    //ajax request for google geocoding api to get location.
 
-    var holderDiv = $("<div>");
+    var long = response.results[i].place.longitude;
 
-    holderDiv.append("<p>" + response.results[i].assetName + "</p>")
+    var lat = response.results[i].place.latitude;
 
-    holderDiv.append("<a href=" + response.results[i].homePageUrlAdr + "> Website</a>")
+    var address = "sucks";
 
-    holderDiv.append("<p>Location of Activity: " + response.results[i].place.longitude + " " + response.results[i].place.latitude+ "</p>")
+    $.ajax({
+      url: "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + long +" &key=AIzaSyDXLr5R_-ztYDx3GcZtuWCS4FsY6JrOa9A",
+      method: "GET"
+    }).then(function (response2) {
+      address = response2.results[0].formatted_address
+      var holderDiv = $("<div>");
+      
 
-    newItem.append(holderDiv);
+      
 
-    // append distance
+      var newItem = $("<div class = 'list-item flex-container'>")
 
-  
+      newItem.append("<img class = 'thumbnail' src =" + response.results[i].assetImages[0].imageUrlAdr + " width ='70px' height = '70px' alt = 'img'>")
 
-    $("#scroll-list").append(newItem)
+     
+
+      holderDiv.append("<p>" + response.results[i].assetName + "</p>")
+
+      holderDiv.append("<a href=" + response.results[i].homePageUrlAdr + "> Website</a>")
+
+      
+
+      newItem.append(holderDiv);
+
+      holderDiv.append("<p>Location: " + address+ "</p>")
+
+
+      $("#scroll-list").append(newItem)
+    })
+
+
+
+    
   }
 
 })
@@ -51,7 +72,7 @@ function getTodaysDate() {
   var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
   var yyyy = today.getFullYear();
 
-  today = yyyy+ '-' + mm + '-' + dd + "..";
+  today = yyyy + '-' + mm + '-' + dd + "..";
   return today
 }
 
